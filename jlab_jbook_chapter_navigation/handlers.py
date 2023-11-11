f2f814391db28f9c5fc0c3373cd1af5a74adc476
Alex-Lewandowski <aflewandowski@alaska.edu>
2023-11-10 16:32:00 -0900

    css working

--- jlab_jbook_chapter_navigation/handlers.py
+++ jlab_jbook_chapter_navigation/handlers.py
@@ -5,7 +5,6 @@ from sphinx_external_toc.parsing import parse_toc_yaml
 
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
-
 from jupyter_server.services.contents.filemanager import FileContentsManager
 
 import tornado
@@ -19,6 +18,8 @@ def get_current_working_directory(current_URL):
 
     if 'tree' in current_URL:
         subfolder = current_URL.split('/tree/')[1]
+        if not Path(subfolder).is_dir:
+            subfolder = str(Path(subfolder).parent)
     else:
         subfolder = ''
 
@@ -44,29 +45,70 @@ def get_title(file_pth):
                     return line[2:].strip()
 
 
-def toc_to_html(toc):
+def toc_to_html(toc, cwd):
+    """
+    Builds a Jupyter-Book Table of Contents as an unordered HTML list.
+    Links to source files for viewing in JupyterLab (not a built html book)
+
+    TODO: add ordered list support for numbered TOCs
+    """
     html = f"{toc.root.docname} <ul>"
     for tree in toc.root.subtrees:
         html = f"{html} <li>{tree.caption}</li><ul>"
-        for item in tree.items:
-            title = get_title(item)
+        for i, branch in enumerate(tree.items):
+            # relative_path = (Path(cwd)/branch).relative_to(Path.cwd())
+            title = get_title(branch)
             if title:
-                html = f'{html} <li><a href={item} target="_blank">{title}</a></li>'
+                html = f'{html} <li><button class="jp-Button toc-button" data-index="{i}">{title}</button></li>'
             else:
-                html = f'{html} <li><a href={item} target="_blank">{item}</a></li>'
-            if len(toc[item].subtrees) > 0:
+                html = f'{html} <li><button class="jp-Button toc-button" data-index="{i}">{branch}</button></li>'
+            if len(toc[branch].subtrees) > 0:
                 html = f"{html} <ul>"
-                for treee in toc[item].subtrees:
-                    for itemm in treee.items:
-                        title = get_title(itemm)
+                for twig in toc[branch].subtrees:
+                    for j, leaf in enumerate(twig.items):
+                        # relative_path = (Path(cwd)/leaf).relative_to(Path.cwd())
+                        title = get_title(leaf)
                         if title:
-                            html = f'{html} <li><a href={itemm} target="_blank">{title}</a></li>'
+                            html = f'{html} <li><button class="jp-Button toc-button" data-index="{i+j+100}">{title}</button></li>'
                         else:
-                            html = f'{html} <li><a href={itemm} target="_blank">{itemm}</a></li>'
+                            html = f'{html} <li><button class="jp-Button toc-button" data-index="{i+j+100}">{leaf}</button></li>'
                 html = f"{html} </ul>"
         html = f"{html} </ul>"
     html = f"{html} </ul>"
     return html 
+
+# def toc_to_html(toc, cwd):
+#     """
+#     Builds a Jupyter-Book Table of Contents as an unordered HTML list.
+#     Links to source files for viewing in JupyterLab (not a built html book)
+
+#     TODO: add ordered list support for numbered TOCs
+#     """
+#     html = f"{toc.root.docname} <ul>"
+#     for tree in toc.root.subtrees:
+#         html = f"{html} <li>{tree.caption}</li><ul>"
+#         for branch in tree.items:
+#             relative_path = (Path(cwd)/branch).relative_to(Path.cwd())
+#             title = get_title(branch)
+#             if title:
+#                 html = f'{html} <li><a href={relative_path}>{title}</a></li>'
+#             else:
+#                 html = f'{html} <li><a href={relative_path}>{branch}</a></li>'
+#             if len(toc[branch].subtrees) > 0:
+#                 html = f"{html} <ul>"
+#                 for twig in toc[branch].subtrees:
+#                     for leaf in twig.items:
+#                         relative_path = (Path(cwd)/leaf).relative_to(Path.cwd())
+#                         title = get_title(leaf)
+#                         if title:
+#                             html = f'{html} <li><a href={relative_path}>{title}</a></li>'
+#                         else:
+#                             html = f'{html} <li><a href={relative_path}>{leaf}</a></li>'
+#                 html = f"{html} </ul>"
+#         html = f"{html} </ul>"
+#     html = f"{html} </ul>"
+#     return html 
+
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -82,7 +124,9 @@ class RouteHandler(APIHandler):
         if len(toc_pth) > 0:
             toc_pth = str(toc_pth[0])
             toc = parse_toc_yaml(toc_pth)
-            html_toc = toc_to_html(toc)
+            # html_toc = toc_to_html(toc, current_URL)
+            html_toc = toc_to_html(toc, cwd)
+            # html_toc = toc_to_html(toc)
         else:
             toc_pth = f"Not a Jupyter-Book: _toc.yml not found in {Path.cwd()}"
             html_toc = f"<p>{toc_pth}</p>"
--- src/index.ts
+++ src/index.ts
@@ -32,13 +32,24 @@ const plugin: JupyterFrontEndPlugin<void> = {
         const data = await requestAPI<any>('get-toc', document.URL);
         console.log(data);
         summary.innerHTML = data['data'];
-        summary.insertAdjacentHTML('afterend', data['cwd']);
+        // summary.insertAdjacentHTML('afterend', data['cwd']);
       } catch (reason) {
         console.error(
           `The jlab_jbook_chapter_navigation server extension appears to be missing.\n${reason}`
         );
       }
     };
+
+    const buttons = document.querySelectorAll('.toc-button');
+        buttons.forEach(button => {
+            button.addEventListener('click', (event: Event) => {
+                const index = button.getAttribute('data-index');
+                console.log(`Button ${index} clicked`);
+                // Add your custom logic here
+            });
+        });
+
+
 
     // Add the widget to the sidebar
     shell.add(widget, 'left', { rank: 100 });
@@ -48,4 +59,80 @@ const plugin: JupyterFrontEndPlugin<void> = {
   },
 };
 
+
+
+
 export default plugin;
+
+
+
+
+// import {
+//   JupyterFrontEnd,
+//   JupyterFrontEndPlugin,
+//   ILabShell
+// } from '@jupyterlab/application';
+// import { requestAPI } from './handler';
+// import { Widget } from '@lumino/widgets';
+
+// const plugin: JupyterFrontEndPlugin<void> = {
+//   id: 'jlab-jbook-chapter-navigation:plugin',
+//   description: 'A JupyterLab extension that mimics jupyter-book chapter navigation on an un-built, cloned jupyter book in JupyterLab.',
+//   autoStart: true,
+//   requires: [ILabShell],
+//   activate: async (
+//     app: JupyterFrontEnd,
+//     shell: ILabShell
+//   ) => {
+//     console.log('JupyterLab extension jlab-jbook-chapter-navigation is activated!');
+
+//     let data: any = null;
+//     try {
+//       data = await requestAPI<any>('get-toc', document.URL);
+//       console.log(data);
+//     } catch (reason) {
+//       console.error(
+//         `The jlab_jbook_chapter_navigation server extension appears to be missing.\n${reason}`
+//       );
+//     }
+
+//     // Create a function to add your widget
+//     function addWidget() {
+//       const widget = new Widget();
+//       widget.id = '@jupyterlab-sidepanel/example';
+//       widget.title.iconClass = "jp-SpreadsheetIcon jp-SideBar-tabIcon";
+//       widget.title.caption = "Side Panel";
+
+//       let summary = document.createElement('p');
+//       widget.node.appendChild(summary);
+//       summary.innerHTML = data['data'];
+//       summary.insertAdjacentHTML("afterend", data['cwd']);
+
+//       shell.add(widget, 'left', { rank: 100 });
+
+//       // Attach the `activate` event handler to the widget
+//       widget.activate = async () => {
+//         console.log('Widget shown');
+//         try {
+//           data = await requestAPI<any>('get-toc', document.URL);
+//           console.log(data);
+//         } catch (reason) {
+//           console.error(
+//             `The jlab_jbook_chapter_navigation server extension appears to be missing.\n${reason}`
+//             ); 
+//           }
+
+//         // You can place your logic here to be executed when the widget is shown.
+//       };
+//     }
+
+//     // Use JupyterLab's `shell` object to detect when the sidebar is shown
+//     shell.currentChanged.connect((_, change) => {
+//       if (change.newValue) {
+//         addWidget(); // Add your widget when the sidebar is shown
+//       }
+//     });
+//   }
+// };
+
+// export default plugin;
