d96bd6a3bd15f1ffed30737e947c829bc776ab28
Alex-Lewandowski <aflewandowski@alaska.edu>
2023-11-12 16:36:19 -0900

    cleaning up odds and ends

--- src/index.ts
+++ src/index.ts
@@ -26,11 +26,9 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
     // Create the widget only once
     const widget = new Widget();
-    widget.id = '@jupyterlab-sidepanel/example';
-    // widget.title.iconClass = 'jp-SpreadsheetIcon jp-SideBar-tabIcon';
+    widget.id = '@jupyterlab-sidepanel/jupyterbook-toc';
     widget.title.iconClass = 'jp-NotebookIcon jp-SideBar-tabIcon';
-
-    widget.title.caption = 'Side Panel';
+    widget.title.caption = 'Jupyter-Book Table of Contents';
 
     let summary = document.createElement('p');
     widget.node.appendChild(summary);
@@ -94,81 +92,8 @@ function addClickListenerToButtons(fileBrowser: FileBrowser | null, docManager: 
 
         const filePath = button.getAttribute('data-file-path');
         if (typeof filePath === 'string') {
-
-          // docManager.openOrReveal("/ASF_SAR_Data_Recipes/burst_stack_to_custom_chunked_zarr.ipynb");
-
-
           docManager.openOrReveal(filePath);
-
         }
       });
   });
 }
-
-
-
-
-
-// import {
-//   JupyterFrontEnd,
-//   JupyterFrontEndPlugin,
-//   ILabShell,
-// } from '@jupyterlab/application';
-// import { requestAPI } from './handler';
-// import { Widget } from '@lumino/widgets';
-
-// const plugin: JupyterFrontEndPlugin<void> = {
-//   id: 'jlab-jbook-chapter-navigation:plugin',
-//   description: 'A JupyterLab extension that mimics jupyter-book chapter navigation on an un-built, cloned jupyter book in JupyterLab.',
-//   autoStart: true,
-//   requires: [ILabShell],
-//   activate: async (app: JupyterFrontEnd, shell: ILabShell) => {
-//     console.log('JupyterLab extension jlab-jbook-chapter-navigation is activated!');
-
-//     // Create the widget only once
-//     const widget = new Widget();
-//     widget.id = '@jupyterlab-sidepanel/example';
-//     widget.title.iconClass = 'jp-SpreadsheetIcon jp-SideBar-tabIcon';
-//     widget.title.caption = 'Side Panel';
-
-//     let summary = document.createElement('p');
-//     widget.node.appendChild(summary);
-
-//     // Attach the `activate` event handler to the widget
-//     widget.activate = async () => {
-//       console.log('Widget shown');
-
-//       // Make the API request and update the widget's content
-//       try {
-//         const data = await requestAPI<any>('get-toc', document.URL);
-//         console.log(data);
-//         summary.innerHTML = data['data'];
-//         // Add the event listener after the widget's content is updated
-//         addClickListenerToButtons();
-//       } catch (reason) {
-//         console.error(
-//           `The jlab_jbook_chapter_navigation server extension appears to be missing.\n${reason}`
-//         );
-//       }
-//     };
-
-//     // Add the widget to the sidebar
-//     shell.add(widget, 'left', { rank: 100 });
-
-//     // Initially trigger the widget's activation
-//     widget.activate();
-//   },
-// };
-
-// export default plugin;
-
-// function addClickListenerToButtons() {
-//   const buttons = document.querySelectorAll('.toc-button');
-//   buttons.forEach((button) => {
-//     button.addEventListener('click', (event: Event) => {
-//       const index = button.getAttribute('data-index');
-//       console.log(`Button ${index} clicked`);
-//       // Add your custom logic here
-//     });
-//   });
-// }
